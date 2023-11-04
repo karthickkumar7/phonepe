46edc29a3f8632595d398b70f36dd7a2a4fc8f3e
karthick <karthickkumar7.k7@gmail.com>
2023-11-04 07:00:07 -0700

    upadated

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var path = require('path');
 var logger = require('morgan');
 var express = require('express');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 var createError = require('http-errors');
 var cookieParser = require('cookie-parser');
 //++++++++++++++++++++++++++++++++++++++++++
@@ -17,9 +17,11 @@ app.set('view engine', 'ejs');
 //++++++++++++++++++++++++++++++++++++++++++
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use(
+    bodyParser.urlencoded({
+        extended: false,
+    })
+);
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 //++++++++++++++++++++++++++++++++++++++++++
@@ -28,24 +30,25 @@ app.use('/users', usersRouter);
 //++++++++++++++++++++++++++++++++++++++++++
 // catch 404 and forward to error handler
 //++++++++++++++++++++++++++++++++++++++++++
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 //++++++++++++++++++++++++++++++++++++++++++
 // error handler
 //++++++++++++++++++++++++++++++++++++++++++
-app.use(function(err, req, res, next) {
-  //++++++++++++++++++++++++++++++++++++++++++
-  // set locals, only providing error in development
-  //++++++++++++++++++++++++++++++++++++++++++
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  //++++++++++++++++++++++++++++++++++++++++++
-  // render the error page
-  //++++++++++++++++++++++++++++++++++++++++++
-  res.status(err.status || 500);
-  res.render('error');
+app.use(function (err, req, res, next) {
+    //++++++++++++++++++++++++++++++++++++++++++
+    // set locals, only providing error in development
+    //++++++++++++++++++++++++++++++++++++++++++
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    //++++++++++++++++++++++++++++++++++++++++++
+    // render the error page
+    //++++++++++++++++++++++++++++++++++++++++++
+    res.status(err.status || 500);
+    res.render('error');
 });
 //++++++++++++++++++++++++++++++++++++++++++
-module.exports = app;
+// module.exports = app;
+app.listen(process.env.PORT || 5000, () => console.log('server started'));
 //++++++++++++++++++++++++++++++++++++++++++
